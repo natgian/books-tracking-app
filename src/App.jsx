3892a8f0c9fecf15737e83e236d4037eb5f94b76
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-06-06 22:05:12 +0200

    Added HTML structure and worked on  CSS of Book component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Searchbar from "./components/Searchbar/Searchbar";
+import Book from "./components/Book/Book";
 import "./index.css";
 
 function App() {
   return (
     <main className="main-layout">
-      <Navbar />
-      <Searchbar />
-      <section></section>
+      <header>
+        <Navbar />
+        <Searchbar />
+      </header>
+      <section className="container">
+        <Book />
+      </section>
       <Footer />
     </main>
   );
--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,4 +1,66 @@
+import "./Book.css";
+
 const Book = () => {
-  return <div>Book</div>;
+  return (
+    <section className="book-details section-container">
+      <div className="book-cover-container">
+        <img
+          src="https://images.thalia.media/00/-/6ca5129269624803a37091db8624a685/die-letzte-nacht-gebundene-ausgabe-karin-slaughter.jpeg"
+          alt=""
+          className="book-cover"
+        />
+        <button>Gelesen</button>
+      </div>
+
+      <div className="book-details-container">
+        <div className="book-header">
+          <h1 className="book-title">Die letzte Nacht</h1>
+          <a href="#" className="book-author">
+            Karin Slaughter
+          </a>
+          <a href="#" className="book-genre">
+            Krimi
+          </a>
+        </div>
+
+        <p className="book-text">
+          Vor fünfzehn Jahren veränderte sich Sara Lintons Leben schlagartig,
+          als sie nach einem Barbesuch brutal überfallen wurde. Mittlerweile hat
+          sie es geschafft, das Trauma hinter sich zu lassen: Sara ist
+          erfolgreiche Ärztin und mit einem Mann verlobt, den sie liebt...
+        </p>
+        <div className="book-details-table">
+          <table>
+            <tbody>
+              <tr>
+                <th>ISBN:</th>
+                <td>978-3-365-00370-1</td>
+              </tr>
+              <tr>
+                <th>Verlag:</th>
+                <td>Haper Collins</td>
+              </tr>
+              <tr>
+                <th>Erscheinungsdatum:</th>
+                <td>21.07.2023</td>
+              </tr>
+              <tr>
+                <th>Seitenzah:</th>
+                <td>560</td>
+              </tr>
+              <tr>
+                <th>Sprache:</th>
+                <td>Deutsch</td>
+              </tr>
+              <tr>
+                <th>Einband:</th>
+                <td>Fester Einband</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Book;
