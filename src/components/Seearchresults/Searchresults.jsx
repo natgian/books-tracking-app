43d580204f6e445b5f2ce70613679def30734087
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-06-25 21:25:03 +0200

    Worked on Searchresults component. Added CSS and defaultCover image. Adjusted data fetching code.

--- src/components/Seearchresults/Searchresults.jsx
+++ src/components/Seearchresults/Searchresults.jsx
@@ -1,23 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { useGlobalContext } from "../../context";
 import axios from "axios";
-
-// const fetchBooks = async () => {
-//   const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
-//   try {
-//     const response = await axios.get(
-//       `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}`
-//     );
-//     const books = response.data.items;
-//     books.forEach((book) => {
-//       console.log(book.volumeInfo);
-//       console.log(`${book.volumeInfo.title}, ${book.volumeInfo.authors}`);
-//     });
-//   } catch (error) {
-//     setIsError(true);
-//     console.log(error.response);
-//   }
-// };
+import "./Searchresults.css";
+import defaultCover from "../../assets/no-cover.jpg";
 
 const Searchresults = () => {
   const { searchTerm } = useGlobalContext();
@@ -28,31 +13,66 @@ const Searchresults = () => {
       const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
 
       const result = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=potter&key=${apiKey}`
+        `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}`
       );
-      return result.data.items;
+      return result.data.items || [];
     },
+    enabled: !!searchTerm, // Only run the query if searchTerm is not empty
   });
 
+  if (!searchTerm) {
+    return null;
+  }
+
   if (isPending) {
-    return <div>Loading...</div>;
+    return <section className="text-center">Loading...</section>;
   }
 
   if (isError) {
     console.log(isError);
-    return <div>Etwas ist schiefgelaufen...</div>;
+    return (
+      <section className="text-center">
+        Etwas ist schiefgelaufen. Bitte erneut versuchen.
+      </section>
+    );
   }
 
   if (data.length < 1) {
-    return <div>Keine Resultate mit diesem Suchbegriff...</div>;
+    return (
+      <section className="text-center">
+        Die Suche ergab leider keine Treffer.
+      </section>
+    );
   }
 
   console.log(data);
 
   return (
-    <section className="section-container">
+    <section className="section-container searchresults-container">
       {data.map((book) => {
-        return <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="" />;
+        return (
+          <a href="#" key={book.id}>
+            <div className="searchresult-wrapper">
+              <div className="searchresult-cover-wrapper">
+                <img
+                  src={
+                    book.volumeInfo?.imageLinks?.smallThumbnail || defaultCover
+                  }
+                  alt={book.volumeInfo.title}
+                />
+              </div>
+              <div className="searchresult-details-wrapper">
+                <p className="searchresult-title">{book.volumeInfo.title}</p>
+                <p className="searchresult-author">
+                  {book.volumeInfo.authors &&
+                    (book.volumeInfo.authors.length > 1
+                      ? book.volumeInfo.authors.join(", ")
+                      : book.volumeInfo.authors)}
+                </p>
+              </div>
+            </div>
+          </a>
+        );
       })}
     </section>
   );
