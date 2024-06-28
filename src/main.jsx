e098f76e745ef3408f4cea0e7329aa928f0f9f70
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-06-28 20:45:55 +0200

    Added Query React Devtools.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
 import { AppProvider } from "./context.jsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <AppProvider>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
     </AppProvider>
   </React.StrictMode>
