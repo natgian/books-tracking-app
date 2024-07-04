0ece7c8393d44a3da4c19cab4709dba09e9ef628
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-07-04 22:51:15 +0200

    Added Searchresults page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as MainLayout } from "./MainLayout";
 export { default as Homepage } from "./Homepage";
 export { default as Book } from "./Book/Book";
+export { default as Searchresults } from "./Seearchresults/Searchresults";
