8e05062ec550fb7be22bcc7b0da674d27ea79c10
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-08-01 18:12:32 +0200

    Added id prop to FormInput and added htmlFor attribute. Modified text in login page and made CSS adjustments.

--- frontend/src/components/Form/FormInput.jsx
+++ frontend/src/components/Form/FormInput.jsx
@@ -1,12 +1,19 @@
 import "../Form/Form.css";
 
-const FormInput = ({ label, name, type }) => {
+const FormInput = ({ id, label, name, type, autocomplete }) => {
   return (
     <div className="form-control">
-      <label htmlFor="email" className="form-label">
+      <label htmlFor={id} className="form-label">
         {label}
       </label>
-      <input type={type} name={name} className="form-input" required />
+      <input
+        id={id}
+        type={type}
+        name={name}
+        autoComplete={autocomplete}
+        className="form-input"
+        required
+      />
     </div>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,12 +13,18 @@ const Login = () => {
 
       <section className="section-container form-container">
         <Form method="POST" className="form">
-          <h1 className="form-title">Login</h1>
+          <h1 className="form-title">Anmelden</h1>
           <div className="underline-title"></div>
-          <FormInput label="email" name="email" type="email" />
-          <FormInput label="password" name="password" type="password" />
+          <FormInput id="email" label="email" name="email" type="email" />
+          <FormInput
+            id="password"
+            label="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+          />
           <div className="flex-center mt-2">
-            <Button text="login" block={true} />
+            <Button text="anmelden" block={true} />
           </div>
           <div className="form-links-container">
             <Link to="/registration" className="form-link">
