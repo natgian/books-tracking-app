654aa4d209d14e88897f1958bfa78cf3569b6070
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-03 22:09:29 +0200

    Created language map object to map the language codes to full language names.

---
+++ frontend/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Buttons/Button";
+export { default as ScrollToTopBtn } from "./Buttons/ScrollToTopBtn";
+export { default as Footer } from "./Footer/Footer";
+export { default as FormInput } from "./Form/FormInput";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Searchbar } from "./Searchbar/Searchbar";
+export { default as Loading } from "./Loading";
---
+++ frontend/src/utilities/languageMap.js
@@ -0,0 +1,10 @@
+export const languageMap = {
+  en: "Englisch",
+  de: "Deutsch",
+  fr: "Französisch",
+  es: "Spanisch",
+  it: "Italienisch",
+  ja: "Japanisch",
+  zh: "Chinesisch",
+  ru: "Russisch",
+};
