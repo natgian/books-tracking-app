33c70fc00c1aaa936217726b2275b83837ce09ab
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-12 10:27:11 +0200

    Added BooklistCard component to BookList.

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,10 +1,12 @@
 import "./BookList.css";
 import SortBtn from "../Buttons/SortBtn";
+import BookListCard from "../BookListCard/BookListCard";
 
 const BookList = () => {
   return (
     <section className="booklist-container">
       <SortBtn />
+      <BookListCard />
     </section>
   );
 };
