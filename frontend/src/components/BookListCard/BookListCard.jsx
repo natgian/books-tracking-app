fccc6424d021826c2a16c568ec253f9e25e8993e
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-13 17:16:25 +0200

    Added cover, title, author, star rating and some CSS to the BookListCard component.

--- frontend/src/components/BookListCard/BookListCard.jsx
+++ frontend/src/components/BookListCard/BookListCard.jsx
@@ -1,13 +1,27 @@
+import StarRating from "../StarRating/StarRating";
 import "./BookListCard.css";
 
 const BookListCard = () => {
   return (
     <div className="booklist-card">
-      <img
-        className="booklist-cover"
-        src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1482945930l/33597147._SY75_.jpg"
-        alt="title"
-      />
+      {/* COVER */}
+      <div>
+        <img
+          className="booklist-cover"
+          src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1482945930l/33597147._SY75_.jpg"
+          alt="title"
+        />
+      </div>
+      {/* DETAILS */}
+      <div>
+        <h2>The Final Gambit</h2>
+        <h3>Jennifer Lynn Barnes</h3>
+        <StarRating
+          averageRating={3}
+          ratingsCount={0}
+          showRatingsCount={false}
+        />
+      </div>
     </div>
   );
 };
