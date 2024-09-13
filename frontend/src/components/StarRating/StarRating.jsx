a3b258c502caecea5ae19aa410e685080ded12fc
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-13 17:18:57 +0200

    Modified StarRatings appearance. Added 'showRatingsCount' prop.

--- frontend/src/components/StarRating/StarRating.jsx
+++ frontend/src/components/StarRating/StarRating.jsx
@@ -1,9 +1,12 @@
 import "./StarRating.css";
-import { BiStar } from "react-icons/bi";
 import { BiSolidStarHalf } from "react-icons/bi";
 import { BiSolidStar } from "react-icons/bi";
 
-const StarRating = ({ averageRating, ratingsCount }) => {
+const StarRating = ({
+  averageRating,
+  ratingsCount,
+  showRatingsCount = true,
+}) => {
   if (averageRating == null || isNaN(averageRating)) {
     return null;
   }
@@ -17,14 +20,16 @@ const StarRating = ({ averageRating, ratingsCount }) => {
           } else if (averageRating >= index + 0.5) {
             return <BiSolidStarHalf key={index} className="star" />;
           } else {
-            return <BiStar key={index} className="star" />;
+            return <BiSolidStar key={index} className="empty-star" />;
           }
         })}
       </div>
-      <div className="ratings">
-        {averageRating} von 5 ({ratingsCount}{" "}
-        {ratingsCount === 1 ? "Bewertung" : "Bewertungen"})
-      </div>
+      {showRatingsCount && (
+        <div className="ratings">
+          {averageRating} von 5 ({ratingsCount}{" "}
+          {ratingsCount === 1 ? "Bewertung" : "Bewertungen"})
+        </div>
+      )}
     </div>
   );
 };
