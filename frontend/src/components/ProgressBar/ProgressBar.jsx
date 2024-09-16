e226ec89ab3bf8dda730b6b3654b7ff03fc69abd
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-16 21:11:04 +0200

    CSS adjustments in progress bar.

--- frontend/src/components/ProgressBar/ProgressBar.jsx
+++ frontend/src/components/ProgressBar/ProgressBar.jsx
@@ -6,13 +6,15 @@ const ProgressBar = () => {
     <div className="progress-bar-container">
       <div className="progress-wrapper">
         <span>Fortschritt:</span>
-        {/* VISUAL BAR */}
-        <div className="progress-bar">
-          <div className="progress"></div>
-        </div>
-        {/* PAGE COUNT */}
-        <div>
-          <p className="progress-bar-pagecount">200/400 (50%)</p>
+        <div className="progress-bar-count-wrapper">
+          {/* VISUAL BAR */}
+          <div className="progress-bar">
+            <div className="progress"></div>
+          </div>
+          {/* PAGE COUNT */}
+          <div>
+            <p className="progress-bar-pagecount">200/400 (50%)</p>
+          </div>
         </div>
       </div>
       {/* UPDATE BUTTON */}
