c15706d4e38a3744f6840f4553437d2eea268765
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-17 22:08:59 +0200

    Added currentList state.

--- frontend/src/pages/ReadingLists/ReadingLists.jsx
+++ frontend/src/pages/ReadingLists/ReadingLists.jsx
@@ -1,8 +1,10 @@
 import "./ReadingLists.css";
+import { useState } from "react";
 
 import { ReadingListsNavbar, BookList, PageTitle } from "../../components";
 
 const ReadingLists = () => {
+  const [currentList, setCurrentList] = useState("reading");
   return (
     <section className="section-container">
       <PageTitle text="Deine Leselisten" lineWidth="14rem" />
