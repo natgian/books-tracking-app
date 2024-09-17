6615b9d742cb33fced431a740d758127df8f041c
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-17 22:10:48 +0200

    Added activeTab state. Added handleTabClick function for tab changes to set current list and add active-tab class dynamically.

--- frontend/src/components/ReadingListsNavbar/ReadingListsNavbar.jsx
+++ frontend/src/components/ReadingListsNavbar/ReadingListsNavbar.jsx
@@ -1,24 +1,53 @@
+import { useState } from "react";
 import "./ReadingListsNavbar.css";
 import { FaBookOpenReader, FaBookBookmark, FaCheck } from "react-icons/fa6";
 
-const ReadingListsNavbar = () => {
+const ReadingListsNavbar = ({ setCurrentList }) => {
+  const [activeTab, setActiveTab] = useState("reading");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+    setCurrentList(tabName);
+  };
+
   return (
     <nav>
       <ul className="reading-lists-nav">
         <li>
-          <button className="reading-list-tab active-tab">
+          <button
+            className={
+              activeTab === "reading"
+                ? "reading-list-tab active-tab"
+                : "reading-list-tab"
+            }
+            onClick={() => handleTabClick("reading")}
+          >
             <FaBookOpenReader size={"3rem"} />
             <span>am Lesen</span>
           </button>
         </li>
         <li>
-          <button className="reading-list-tab">
+          <button
+            className={
+              activeTab === "tbr"
+                ? "reading-list-tab active-tab"
+                : "reading-list-tab"
+            }
+            onClick={() => handleTabClick("tbr")}
+          >
             <FaBookBookmark size={"3rem"} />
             <span>zum Lesen</span>
           </button>
         </li>
         <li>
-          <button className="reading-list-tab">
+          <button
+            className={
+              activeTab === "read"
+                ? "reading-list-tab active-tab"
+                : "reading-list-tab"
+            }
+            onClick={() => handleTabClick("read")}
+          >
             <FaCheck size={"3rem"} />
             <span>gelesen</span>
           </button>
