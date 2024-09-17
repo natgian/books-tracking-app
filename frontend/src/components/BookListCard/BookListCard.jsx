46166357418bd238b1e8d3eaf45d2d228bd2ae83
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-17 22:11:58 +0200

    Added props for conditional rendering of dates and progress bar.

--- frontend/src/components/BookListCard/BookListCard.jsx
+++ frontend/src/components/BookListCard/BookListCard.jsx
@@ -2,7 +2,11 @@ import ProgressBar from "../ProgressBar/ProgressBar";
 import StarRating from "../StarRating/StarRating";
 import "./BookListCard.css";
 
-const BookListCard = () => {
+const BookListCard = ({
+  showProgressBar = true,
+  showReadDate = false,
+  readDate = "",
+}) => {
   return (
     <div className="booklist-card">
       {/* COVER */}
@@ -15,24 +19,34 @@ const BookListCard = () => {
       </div>
 
       <div className="booklist-card-info-wrapper">
-        {/* DETAILS */}
-        <div>
-          <h2 className="booklist-card-title">Not Another Love Song</h2>
-          <h3 className="booklist-card-author">Jennifer Lynn Barnes</h3>
-          <StarRating
-            averageRating={3}
-            ratingsCount={0}
-            showRatingsCount={false}
-          />
-          {/* DATE ADDED */}
-          <div className="booklist-card-date">
-            <span>Hinzugefügt am:</span>
-            <p>08.08.2024</p>
-          </div>
+        {/* BOOK INFOS */}
+
+        <h2 className="booklist-card-title">Not Another Love Song</h2>
+        <h3 className="booklist-card-author">Jennifer Lynn Barnes</h3>
+        <StarRating
+          averageRating={3}
+          ratingsCount={0}
+          showRatingsCount={false}
+        />
+      </div>
+
+      {/* DATES & PROGESS */}
+      <div className="booklist-card-progress-wrapper">
+        {/* DATE ADDED */}
+        <div className="booklist-card-date">
+          <span>Hinzugefügt am:</span>
+          <p>08.08.2024</p>
         </div>
+        {/* DATE READ */}
+        {showReadDate && (
+          <div className="booklist-card-date">
+            <span>Zu Ende gelesen am:</span>
+            <p>15.08.2024</p>
+          </div>
+        )}
 
         {/* PROGRESS */}
-        <ProgressBar />
+        {showProgressBar && <ProgressBar />}
       </div>
     </div>
   );
