9d8ec8f6ce07320c8b448baaf5c0be0ce805c14c
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-19 22:33:48 +0200

    Added mongodb database connection.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+const dbURI = process.env.DB_URI;
+const PORT = 3000;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(dbURI);
+  console.log("connected to MongoDB database");
+}
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is working!");
 });
-
-const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}`);
