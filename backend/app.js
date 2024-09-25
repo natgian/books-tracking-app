2a32333cff433af117d21b1c6d5188adcb310704
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-25 20:11:35 +0200

    Changed PORT to ENV variable. Added global middleware to log the path and methods.

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,23 @@ if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
-const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
+// EXPRESS APP
+const app = express();
+
+// GLOBAL MIDDLEWARE
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// ROUTES
 app.get("/", (req, res) => {
   res.send("Server is working!");
 });
 
+// LISTENING FOR REQUESTS
 app.listen(PORT, () => {
   connectDB();
   console.log(`Running on PORT ${PORT}`);
