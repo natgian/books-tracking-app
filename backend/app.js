4d54a9d46cf610ebee3bcdd7982f673aa091a01a
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-26 22:01:15 +0200

    Added express.json middleware and readingListsRoute.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import readingListsRoutes from "./routes/readinglists.js";
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
@@ -12,15 +13,16 @@ const PORT = process.env.PORT;
 const app = express();
 
 // GLOBAL MIDDLEWARE
+// parsing the body the get access to req.body:
+app.use(express.json());
+// used to track incoming requests and their methods in development:
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // ROUTES
-app.get("/", (req, res) => {
-  res.send("Server is working!");
-});
+app.use("/api/readinglists", readingListsRoutes);
 
 // LISTENING FOR REQUESTS
 app.listen(PORT, () => {
