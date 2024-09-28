3246111030c7d7859d5596d34e14052b26e01ed0
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-09-28 15:44:27 +0200

    Refactored app.js file (created separate files for sessonConfig and passportConfig).

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,49 @@
 import express from "express";
-import dotenv from "dotenv";
+import session from "express-session";
+import { sessionConfig } from "./config/sessionConfig.js";
+import { passportConfig } from "./config/passportConfig.js";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
-import readingListsRoutes from "./routes/readinglists.js";
+import mongoSanitize from "express-mongo-sanitize";
+import userRoutes from "./routes/userRoutes.js";
+import readinglistRoutes from "./routes/readinglistRoutes.js";
+import dotenv from "dotenv";
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
 const PORT = process.env.PORT;
+const SECRET = process.env.SECRET;
+const DB_URI = process.env.DB_URI;
 
 // EXPRESS APP
 const app = express();
 
-// GLOBAL MIDDLEWARE
-// parsing the body the get access to req.body:
-app.use(express.json());
-// used to track incoming requests and their methods in development:
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+// Enable CORS for requests coming from frontend
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Set your frontend production domain
+  })
+);
+
+// MIDDLEWARE SETUP
+app.use(express.json()); // parsing the body the get access to req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize);
+
+// Sessions
+app.use(session(sessionConfig(DB_URI, SECRET)));
+
+// Passport
+passportConfig(app);
 
 // ROUTES
-app.use("/api/readinglists", readingListsRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/readinglist", readinglistRoutes);
 
-// LISTENING FOR REQUESTS
+// LISTENING FOR REQUESTS & CONNECTING DO DATABASE
 app.listen(PORT, () => {
-  connectDB();
+  connectDB(DB_URI);
   console.log(`Running on PORT ${PORT}`);
 });
