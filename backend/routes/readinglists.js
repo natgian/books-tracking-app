2ec6316cdb0c768d0940a4c256cc897bea8ef1ca
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-10-03 21:15:31 +0200

    Deleted unused files.

--- backend/routes/readinglists.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// GET all lists
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET lists page" });
-});
-
-export default router;
