599948f817098b8f2c8149de2854c37d786e2b66
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-10-07 22:11:10 +0200

    Created useLogout hook.

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,20 @@
+import backendAxiosConfig from "../api/backendAxiosConfig.js";
+import { useAuthContext } from "./useAuthContext.js";
+import { useNavigate } from "react-router-dom";
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      await backendAxiosConfig.post("/user/logout");
+      dispatch({ type: "LOGOUT" });
+      navigate("/");
+    } catch (error) {
+      console.log("Error logging out:", error);
+    }
+  };
+
+  return { logout };
+};
