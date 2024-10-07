1a43a1964c0f9627c397da643c1fccfab63df548
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-10-07 22:16:44 +0200

    Added handleSubmit to Registration page.

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { useRegister } from "../hooks/useRegister";
 import { Navbar, Footer, FormInput, Button, PageTitle } from "../components";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
 
 const Registration = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const { register, error, isLoading } = useRegister();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // prevents from refreshing the page
+
+    const formData = new FormData(event.currentTarget);
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm-password");
+
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwörter stimmen nicht überein.");
+      return;
+    }
+
+    const newUser = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password,
+    };
+
+    const { success, error: registrationError } = await register(newUser);
+
+    if (success) {
+      navigate("/");
+    } else {
+      setErrorMessage(registrationError);
+    }
+  };
+
   return (
     <main className="main-layout">
       <header>
@@ -9,7 +42,7 @@ const Registration = () => {
       </header>
 
       <section className="section-container form-container">
-        <Form method="POST" className="form">
+        <Form method="POST" className="form" onSubmit={handleSubmit}>
           <PageTitle text="Registrieren" lineWidth="10rem" />
           {/* INPUTS */}
           <FormInput
@@ -40,10 +73,24 @@ const Registration = () => {
             type="password"
             autocomplete="new-password"
           />
+
+          {/* ERROR MESSAGE */}
+          {errorMessage && (
+            <p className="error-message" style={{ color: "red" }}>
+              {errorMessage}
+            </p>
+          )}
+
           {/* REGISTER BUTTON */}
           <div className="flex-center mt-2">
-            <Button text="registrieren" block={true} />
+            <Button
+              type="submit"
+              text="registrieren"
+              block={true}
+              disabled={isLoading}
+            />
           </div>
+
           {/* LINKS */}
           <div className="form-links-container">
             <Link to="/login" className="form-link">
@@ -52,6 +99,7 @@ const Registration = () => {
           </div>
         </Form>
       </section>
+
       <Footer />
     </main>
   );
