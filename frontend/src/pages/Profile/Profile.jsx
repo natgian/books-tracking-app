294f1a1d6d314ce6d501a0230c00ecf9480cdddb
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-10-08 23:07:24 +0200

    Added user data to profile page.

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
 import { PageTitle } from "../../components";
+import { AuthContext } from "../../context/AuthContext";
 
 const Profile = () => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <p>Loading user data...</p>; // This will show while user is not available
+  }
+
   return (
     <section className="section-container">
       <PageTitle text="Dein Profil" lineWidth="8rem" />
+      <div>
+        <p>Benutzername: {user.username}</p>
+        <p>Email: {user.email}</p>
+      </div>
     </section>
   );
 };
