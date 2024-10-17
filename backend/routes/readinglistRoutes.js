977ced1c3e615a3e9c947df6921a33fc7de9eee2
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-10-17 23:33:18 +0200

    Created readinglists routes.

--- backend/routes/readinglistRoutes.js
+++ backend/routes/readinglistRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
 const router = express.Router();
+import {
+  addOrUpdateBookToList,
+  removeBookFromList,
+  getReadingLists,
+} from "../controllers/readingListsController.js";
 
-// GET all lists
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET lists page" });
-});
+// GET ALL USER'S BOOK LISTS
+router.get("/:userId/books", getReadingLists);
+
+// ADD OR UPDATE A BOOK IN USER LIST
+router.post("/:userId/books", addOrUpdateBookToList);
+
+// REMOVE A BOOK FROM USER'S BOOK LIST
+router.delete("/:userId/books/:bookId", removeBookFromList);
 
 export default router;
