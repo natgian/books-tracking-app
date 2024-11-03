5964f9c0e40f1307e7c1cbd9823e6fbc7c0cac4a
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-11-03 23:20:47 +0100

    Changed configurations.

--- backend/config/sessionConfig.js
+++ backend/config/sessionConfig.js
@@ -25,7 +25,7 @@ export const sessionConfig = (DB_URI, SECRET) => {
     cookie: {
       httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
       secure: true, // set to "true" for production
-      sameSite: "lax",
+      sameSite: "none", // "lax" on development
       maxAge: 1000 * 60 * 60 * 12, // Sets maximum age for the cookie to 12 hours
       expires: new Date(Date.now() + 1000 * 60 * 60 * 12), // Sets cookie expiration to 12 hours
     },
