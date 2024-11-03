13536facf81ed217c5904a64aa8a77450e56a88a
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-11-03 22:01:26 +0100

    Modified backendURL.

--- frontend/src/api/backendAxiosConfig.js
+++ frontend/src/api/backendAxiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendURL = process.env.BACKEND_URL || "http://localhost:3000";
+const backendURL = process.env.BACKEND_URL;
 
 const backendAxiosConfig = axios.create({
   baseURL: `${backendURL}/api`,
