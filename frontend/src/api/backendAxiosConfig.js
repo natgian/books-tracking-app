4c258c290a322e5ab896126b7b5fd87aaaf4012b
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-11-08 18:44:01 +0100

    Modified backend environment variables for development and production.

--- frontend/src/api/backendAxiosConfig.js
+++ frontend/src/api/backendAxiosConfig.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// for production:
 const backendURL = import.meta.env.VITE_BACKEND_URL;
-// for development:
-// const backendURL = "http://localhost:3000";
 
 const backendAxiosConfig = axios.create({
   baseURL: `${backendURL}/api`,
