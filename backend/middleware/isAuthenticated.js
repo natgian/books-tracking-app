fbc6b1a746bae9760a6e88c7eea6071304f2d97b
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-11-09 12:04:25 +0100

    Test fixing authentication bug.

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,7 +1,7 @@
 export const isAuthenticated = (req, res, next) => {
   // Verify authentication
   if (req.session && req.session.passport.user) {
-    next(); // user is authenticated, proceed to the next middleware or route
+    return next(); // user is authenticated, proceed to the next middleware or route
   } else {
     return res.status(401).json({
       message:
