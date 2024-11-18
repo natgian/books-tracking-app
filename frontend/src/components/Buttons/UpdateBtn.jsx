69dbc40d1b6cb78db85d35bda0463cd97e5d9205
Nathalie Giancaspro <n.giancaspro@outlook.com>
2024-11-18 21:36:18 +0100

    Small CSS modifications.

--- frontend/src/components/Buttons/UpdateBtn.jsx
+++ frontend/src/components/Buttons/UpdateBtn.jsx
@@ -19,9 +19,7 @@ const UpdateBtn = ({ text, block, onClick }) => {
   return (
     <button
       type="button"
-      className={`update-btn ${block ? "btn-block" : ""} ${
-        isMobile ? "update-btn-small" : ""
-      }`}
+      className={`update-btn ${block ? "btn-block" : ""}`}
       onClick={onClick}
     >
       {text}
