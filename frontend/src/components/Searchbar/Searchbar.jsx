f0401fe1432236e57b2326d73c5c73b3e24fbb96
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-05-16 10:57:24 +0200

    feat: trigger search only on submit instead of on every keystroke

--- frontend/src/components/Searchbar/Searchbar.jsx
+++ frontend/src/components/Searchbar/Searchbar.jsx
@@ -1,36 +1,16 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { Form, useNavigate } from "react-router-dom";
-import { debounce } from "lodash";
 import { BiSearch } from "react-icons/bi";
 import "./Searchbar.css";
-import "tippy.js/dist/tippy.css";
 
 const Searchbar = () => {
   const [inputValue, setInputValue] = useState("");
   const navigate = useNavigate();
 
-  // Memoize the debounced function using useCallback
-  const debouncedNavigate = useCallback(
-    debounce((value) => {
-      if (value) {
-        navigate(`/suchresultate?q=${value}`);
-      }
-    }, 500),
-    [] // The dependencies array is empty, so this function is only created once
-  );
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setInputValue(value);
-    if (value) {
-      debouncedNavigate(value);
-    }
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (inputValue) {
-      navigate(`/suchresultate?q=${inputValue}`);
+    if (inputValue.trim()) {
+      navigate(`/suchresultate?q=${inputValue.trim()}`);
     }
   };
 
@@ -44,7 +24,7 @@ const Searchbar = () => {
         aria-label="Suchbegriff eingeben"
         className="searchbar-input"
         value={inputValue}
-        onChange={handleChange}
+        onChange={(e) => setInputValue(e.target.value)}
       />
 
       <button type="submit" className="searchbar-btn" aria-label="suchen">
