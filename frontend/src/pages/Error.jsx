62c238fb16a65cc777c8236a52fbabaae6276b65
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-06-04 15:28:17 +0200

    feat(ui): add navbar and footer to 404 page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -2,24 +2,27 @@ import { Link, useRouteError } from "react-router-dom";
 import { BiErrorCircle } from "react-icons/bi";
 import notFoundImage from "../assets/page-not-found.svg";
 import errorImage from "../assets/error.svg";
+import { Navbar, Footer } from "../components";
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
 
   if (error.status === 404) {
     return (
-      <section className="error-container">
-        <img
-          src={notFoundImage}
-          alt="Error 404 - Seite nicht gefunden"
-          className="error-image"
-        />
-        <p className="error-title mt-2 mb-1">Hoppla! Seite nicht gefunden</p>
-        <Link to="/" className="error-link">
-          Zurück zur Startseite
-        </Link>
-      </section>
+      <main className="main-layout">
+        <header>
+          <Navbar />
+        </header>
+
+        <section className="error-container">
+          <img src={notFoundImage} alt="Error 404 - Seite nicht gefunden" className="error-image" />
+          <p className="error-title mt-2 mb-1">Hoppla! Seite nicht gefunden</p>
+          <Link to="/" className="error-link">
+            Zurück zur Startseite
+          </Link>
+        </section>
+        <Footer />
+      </main>
     );
   }
 
