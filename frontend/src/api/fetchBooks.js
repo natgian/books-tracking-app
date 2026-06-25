f5ec8ff60f78d38cc66121d92cce4a6bad6dcf1a
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-06-25 19:43:56 +0200

    feat: add buildQuery function to handle saerch type prefixes

--- frontend/src/api/fetchBooks.js
+++ frontend/src/api/fetchBooks.js
@@ -5,18 +5,39 @@ const DEFAULT_START_INDEX = 0;
 const DEFAULT_MAX_RESULTS = 12;
 
 /**
+ * Builds the query strin with the search term depending on the search type
+ *
+ * @param {string} searchTerm - The search term or ISBN to search for
+ * @param {string} searchType - The search type ("all", "title", "author" or "isbn")
+ * @returns - A string with the query
+ */
+const buildQuery = (searchTerm, searchType) => {
+  switch (searchType) {
+    case "isbn":
+      return `isbn:${searchTerm}`;
+    case "title":
+      return `intitle:"${searchTerm}"`;
+    case "author":
+      return `inauthor:"${searchTerm}"`;
+    default:
+      if (isValidISBN(searchTerm)) return `isbn:${searchTerm}`;
+      return searchTerm;
+  }
+};
+
+/**
  * Fetches books by search term or ISBN.
  * If the search term is a valid ISBN, it searches by ISBN directly, otherwise it performs
  * a search in title or author.
  *
  * @param {string} searchTerm - The search term or ISBN to search for
+ * @param {string} searchType - The search type ("all", "title", "author" or "isbn")
  * @param {number} startIndex - The start search index
  * @param {number} maxResults - The maximum number of results to return
  * @returns {Promise<Array>} - A list of books or an empty array
  */
-export const fetchBooks = async (searchTerm, startIndex = DEFAULT_START_INDEX, maxResults = DEFAULT_MAX_RESULTS) => {
-  const isISBN = isValidISBN(searchTerm);
-  const query = isISBN ? `isbn:${searchTerm}` : `intitle:"${searchTerm}" OR inauthor:"${searchTerm}"`;
+export const fetchBooks = async (searchTerm, searchType, startIndex = DEFAULT_START_INDEX, maxResults = DEFAULT_MAX_RESULTS) => {
+  const query = buildQuery(searchTerm, searchType);
 
   try {
     const result = await googleAxiosConfig.get("/volumes", {
